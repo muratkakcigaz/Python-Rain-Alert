9086392117eeada817155502a2be1380f71359d5
muratkakcigaz <mkakcigaz@gmail.com>
2025-12-04 16:13:44 +0300

    added mail protokol

--- main.py
+++ main.py
@@ -2,8 +2,43 @@ import requests
 import os
 import dotenv
 
+#For sending mail#
+import smtplib
+
+
+API_KEY=os.getenv('API_KEY')
+EMAIL=os.getenv('EMAIL')
+EMAIL_PASSWORD=os.getenv('EMAIL_PASSWORD')
+
 def configure():
     dotenv.load_dotenv()
+
+def send_mail(to, subject, body):
+    with smtplib.SMTP(host="smtp@gmail.com",port=587,) as connection:
+        connection.starttls()
+
+        connection.login(user=EMAIL,password=EMAIL_PASSWORD)
+
+        message= f"Subject:{subject}\n\n{body}"
+
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=to,
+            msg=message
+        )
+
+def will_it_rain(data):
+    for x in range(0,4):
+        if data["list"][x]["weather"][0]["id"]<700:
+            print(data["list"][x]["weather"][0]["id"])
+            will_rain=True
+    if will_rain:
+        send_mail(
+            to="",
+            subject="",
+            body=""
+        )
+
 
 
 
@@ -13,7 +48,7 @@ def main():
     parameters={
         "lat":40.978796,
         "lon":28.719360,
-        "appid":os.getenv('API_KEY'),
+        "appid":os.getenv(API_KEY),
         "cnt":4,#count of timestamps 3-6-9-12 #limiting timestamp to near future
     }
     response=requests.get(url=ENDPOINT,params=parameters)
@@ -21,5 +56,7 @@ def main():
     print(response.status_code)
     data=response.json()
     print(data)
+    will_it_rain(data=data)
+
 
 main()
